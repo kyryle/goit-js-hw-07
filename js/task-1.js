6554efe73909f00a03dd822f4e7b8b32cdcaf358
user <kyryle0110@gmail.com>
2026-05-29 11:45:18 +0300

    wrong try 1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+const liItemCount = document.querySelectorAll('#categories>li.item')
+console.log(liItemCount.length)
+
+const result = document.querySelectorAll('#categories>li.item').forEach((element, index) => {
+    const h2 = element.querySelector('h2');
+    console.log(h2.textContent)
+    const liLength = element.querySelectorAll('ul>li')
+    console.log(liLength.length)
+})
+
+
+
+
+
+
+
+

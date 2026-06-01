dff6bf54fa2f70c66d9cc2ffc9bfe1e49864e26a
user <kyryle0110@gmail.com>
2026-06-01 16:19:10 +0300

    done

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,18 @@
-const formListener = document.querySelector('button.submit')
+const formListener = document.querySelector('form.login-form')
 
 formListener.addEventListener('submit', event => {
     event.preventDefault()
     const formCollector = new FormData(formListener)
-    const email = formCollector.get('email').trim()
-    const password = formCollector.get('password').trim()
-    if (email.length === 0 || password.length === 0) {
-        alert('All form fields must be filled in')
-        return
-    }
+    
 
     const data = {
         email: formCollector.get('email').trim(),
         password: formCollector.get('password').trim(),
                 
+    }
+    if (data.email.length === 0 || data.password.length === 0) {
+        alert('All form fields must be filled in')
+        return
     }
     console.log(data)
     formListener.reset()
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonClick = document.querySelector('button.change-color')
+
+const spanFind = document.querySelector('span.color')
+
+const bodyColor = document.querySelector('body')
+
+buttonClick.addEventListener('click', () => {
+  spanFind.textContent = getRandomHexColor()
+  bodyColor.style.backgroundColor = getRandomHexColor()
+})
